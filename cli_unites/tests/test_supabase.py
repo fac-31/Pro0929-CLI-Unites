5e8dd41e83ee07ca7509735c2fd587588107c8be
Jaz-spec <jaz.edna@proton.me>
2025-10-04 16:34:05 +0100

    feat: adds supabase migrations

--- cli_unites/tests/test_supabase.py
+++ cli_unites/tests/test_supabase.py
@@ -1,15 +1,32 @@
 import pytest
-from supabase.create_client import supabase
+from cli_unites.database.create_client import supabase
 
-class TestSupabaseConnection:
 
-    @pytest.fixture
-    def supabase_client(self):
-        """Setup: Create client before each test"""
-        try:
-            # Try to get auth session (doesn't require tables)
-            response = supabase.auth.get_session()
-            print("Connected to Supabase!")
-            return response
-        except Exception as e:
-            print(f"Connection failed: {e}")
+def test_supabase_client():
+    """Setup: Create client before each test"""
+    try:
+        # Try to get auth session (doesn't require tables)
+        print(" ")
+        print("======Connecting to Supabase======")
+        response = supabase.auth.get_session()
+        print(f"Response: {response}")
+        assert response is not None
+    except Exception as e:
+        print(f"Connection failed: {e}")
+        pytest.fail(f"Connection failed: {e}")
+
+# def test_supabase_connection():
+#     """Test Supabase connection"""
+#     try:
+#         print(" ")
+#         print("======Connecting to Supabase======")
+#         response = (
+#             supabase.table("connect")
+#             .select("*")
+#             .execute()
+#         )
+#         print(f"Response: {response}")
+#         #assert response is not None
+#     except Exception as e:
+#         print(f"Connection failed: {e}")
+#         pytest.fail(f"Connection failed: {e}")
